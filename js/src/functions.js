e3745d4fbebd0b6b7373f02989ab657ee671c3c5
login2030 <login2030@gmail.com>
2016-10-21 00:02:27 +0500

    Скомпонавал js библиотеки в один файл + из js материала удалил все лишнее, оставил только нужное. в табах сделал чтобы картинка видоса была видна на мобильной версии

--- js/src/functions.js
+++ js/src/functions.js
@@ -1,4 +1,11 @@
 /* global app, Handlebars */
+/*
+	<script src="js/plugins/modernizr.min.js" type="text/javascript"></script>
+	<script src="https://cdn.jsdelivr.net/jquery/3.1.1/jquery.min.js" type="text/javascript"></script>
+	<script src="https://cdn.jsdelivr.net/handlebarsjs/4.0.5/handlebars.min.js" type="text/javascript"></script>
+	<script src="https://cdn.jsdelivr.net/page.js/1.6.3/page.min.js" type="text/javascript"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
+*/
 function getContent(page, tpl) {
 	page.html(Handlebars.compile(tpl.html())(app.data));
 }
